84597baad527825ef39b5d4a0cce69e333a35db7
georgembugua <georgembugua2@student.moringaschool.com>
2025-05-21 11:21:15 +0300

    complete lab

--- debug.py
+++ debug.py
@@ -2,20 +2,20 @@ from customer import Customer
 from coffee import Coffee
 from order import Order
 
-# Create some customers
+
 customer1 = Customer("Alice")
 customer2 = Customer("Bob")
 
-# Create some coffee types
+
 coffee1 = Coffee("Espresso")
 coffee2 = Coffee("Latte")
 
-# Create some orders
+
 order1 = customer1.create_order(coffee1, 4.5)
 order2 = customer2.create_order(coffee2, 5.0)
 order3 = customer1.create_order(coffee2, 6.0)
 
-# Debugging outputs
+
 print("Customer 1 Orders:", customer1.orders())
 print("Customer 1 Coffees:", [coffee.name for coffee in customer1.coffees()])
 print("Coffee 1 Orders:", coffee1.num_orders())
